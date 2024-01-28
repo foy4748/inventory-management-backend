c84ea7c44ff8b9501e094f0d06207341425016fc
foy4748 <faisaljfcl@gmail.com>
2024-01-28 21:53:11 +0600

    Handled Stack reduction logic

--- src/app/modules/sale/sale.service.ts
+++ src/app/modules/sale/sale.service.ts
@@ -2,10 +2,27 @@
 import ISale from './sale.interface';
 import Sale from './sale.model';
 import { defaultPage, defaultLimit } from '../product/product.constant';
+import Product from '../product/product.model';
+import AppError from '../../error/AppError';
+import httpStatus from 'http-status';
+import mongoose from 'mongoose';
+
 //import AppError from '../../error/AppError';
 
 // Create a New Sale
 export const ScreateSale = async (payload: ISale) => {
+  const { product_id, quantity: saleQuantity } = payload;
+  const toBeSalingProduct = await Product.findById(product_id);
+
+  const updatedStock = Number(toBeSalingProduct?.quantity) - saleQuantity;
+  // Checking whether sale is allowed or not
+  if (updatedStock < 0) {
+    new AppError(httpStatus.FORBIDDEN, "Can't sale exceeding stock");
+  }
+  await Product.updateOne(
+    { _id: new mongoose.Types.ObjectId(product_id) },
+    { $set: { quantity: updatedStock } },
+  );
   const response = await Sale.create(payload);
   return response;
 };
